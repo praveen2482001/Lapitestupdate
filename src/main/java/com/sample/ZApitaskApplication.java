89f230922e3db84a901759218483441a3ece5906
praveen2482001 <praveendavid2482001@gmail.com>
2023-10-04 18:30:29 +0530

    first update

---
+++ src/main/java/com/sample/ZApitaskApplication.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZApitaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZApitaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/ZApitaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZApitaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
